317e2803f0216bde3ea6ce69f29eca49e71a6cb5
Angelo  Menti <angelo.omarzinho@outlook.com>
2022-10-08 23:44:59 -0300

    page - init registration page

--- src/pages/registration/index.tsx
+++ src/pages/registration/index.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
+import { MainRegistration, SearchBox, TableGrid } from './styles';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 
-// import { Container } from './styles';
+
 
 interface RegistrationProps {
   setPositionSelected: React.Dispatch<React.SetStateAction<string>>
@@ -11,7 +13,39 @@ const Registration: React.FC<RegistrationProps> = ({ setPositionSelected }) => {
     setPositionSelected("185px")
   }, [])
 
-  return <div />;
+
+  const columnsTable: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'name', headerName: 'Nome', width: 130 },
+    { field: 'input', headerName: 'Entrada', width: 90 },
+    { field: 'output', headerName: 'Saida', width: 90 },
+    { field: 'last', headerName: 'Ultimo', width: 90 },
+    { field: 'reason', headerName: 'Motivo', width: 130 }
+  ]
+
+  // Still data static
+  const rowsTable = [
+    { id: 1, name: 'Angelo', input: '1200', output: '200', last: '-100', reason: 'Coffee :)' },
+    { id: 2, name: 'Angelo', input: '1000', output: '200', last: '-200', reason: 'Coffee :)' }
+  ]
+
+  return (
+    <MainRegistration>
+      <SearchBox>
+
+      </SearchBox>
+
+      <TableGrid>
+        <DataGrid
+          rows={rowsTable}
+          columns={columnsTable}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+        />
+      </TableGrid>
+    </MainRegistration>
+  )
 }
 
 export default Registration;
--- src/pages/registration/styles.ts
+++ src/pages/registration/styles.ts
@@ -1,1 +1,24 @@
 import styled from "styled-components";
+
+
+export const MainRegistration = styled.div`
+  width: 100%; 
+  height: 100%; 
+  background-color: #e7e4e4; 
+  border-top-left-radius: 30px; 
+  border-bottom-left-radius: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const SearchBox = styled.section`
+  height: 300px;
+  max-width: 1000px;
+  width: 80%;
+`
+
+export const TableGrid = styled.section`
+  width: 80%;
+  height: 400px;
+`
